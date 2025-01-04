712e988591e1cc7cb3d6e97ec4d06716f83116b9
Diego Steiner <diego.steiner@u041.ch>
2025-01-04 11:28:44 +0100

    update prices 2025

--- index.js
+++ index.js
@@ -1,55 +1,66 @@
-var PriceCalculator = Vue.component('price-calculator', {
-	template: `
-   <div class="grid">
-    <label class="label">Art:</label>
-    <div>
-      <input type="radio" id="occupancy_type_camp" value="camp" v-model="occupancy_type">
-      <label for="occupancy_type_camp">Lager/Kurs</label>
-      <br>
-
-      <input type="radio" id="occupancy_type_festivity" value="festivity" v-model="occupancy_type">
-      <label for="occupancy_type_festivity">Fest</label>
-    </div>
-
-    <label for="nights" class="label"># Nächte:</label>
-    <div>
-      <input min="1" max="30" type="number" name="nights" size="4" v-model="nights">
-    </div>
-
-    <label for="people_t1" class="label"># Kinder / Jugendliche:</label>
-    <div>
-      <input type="number" min="0" max="28" required="required" name="people_t1"  v-model="people_t1">
-    </div>
-
-    <label for="people_t2" class="label"># Erwachsene (ab 25 Jahren):</label>
-    <div>
-      <input type="number" min="0" max="20" required="required" name="people_t2" v-model="people_t2">
-    </div>
-
-    <label class="label">unverbindliche Preisschätzung:</label>
-    <div><strong>{{ price_formatted }}</strong></div>
-
-    <p style="font-size: 0.7em;">
-      Es wird nach der Belegung mit den effektiv entstandenen Kosten abgerechnet (Personen/Nebenkosten/Schäden am
-      Haus). Diese variieren bei den Nebenkosten je nach Hausnutzung und Jahreszeit. Unser Preisrechner rechnet mit dem
-      Durchschnitt der Nebenkosten
-      der vergangenen Belegungen. Beachten Sie deshalb, dass wir so nur eine unverbindliche Preisschätzung und
-      keine verbindliche Offerte abbilden können.
-    </p>
-  </div>
-  `,
-  props: {
-  	home_rates: Object
-  },
-  data() {
-  	return {
-      nights: 3,
-      occupancy_type: 'camp',
-      people_t1: 12,
-      people_t2: 4,
-      price_factor: 1.05,
-      home: 'birchli',
+new Vue({
+  el: '#pricecalc',
+  data: {
+    nights: 2,
+    occupancy_type: 'camp',
+    people_t1: 12,
+    people_t2: 4,
+    price_factor: 1.05,
+    home: 'birchli',
+    home_rates: {
+      birchli: {
+        name: 'Pfadiheim Birchli',
+        people_min: 12,
+        people_max: 32,
+        rate_t1: 16.0,
+        rate_t2: 22.0,
+        tax_t1: 0.65,
+        tax_t2: 1.3,
+        additional_costs_night: 1.3,
+        rate_flat: 100.0,
+        fixed_price: [
+          360.0,
+          625.0,
+          985.0
+        ]
+      },
+      villa: {
+        name: 'Pfadiheim Villa Kunterbunt',
+        people_min: 25,
+        people_max: 50,
+        rate_t1: 16.0,
+        rate_t2: 22.0,
+        tax_t1: 1.5,
+        tax_t2: 3.0,
+        additional_costs_night: 2.0,
+        rate_flat: 120.0,
+        fixed_price: [
+          560.0,
+          975.0,
+          1540.0
+        ]
+      },
+      muehlebaechli: {
+        name: 'Pfadiheim Mühlebächli',
+        people_min: 12,
+        people_max: 31,
+        rate_t1: 15.0,
+        rate_t2: 21.0,
+        tax_t1: 1.5,
+        tax_t2: 3.0,
+        additional_costs_night: 1.45,
+        rate_flat: 100.0,
+        fixed_price: [
+          320.0,
+          560.0,
+          885.0
+        ]
+      }
     }
+  },
+  beforeMount: function () {
+    var home = this.$el.attributes['data-home'].value;
+    this.rates = this.home_rates[home];
   },
   watch: {
     people_t1: function (value) {
@@ -64,35 +75,35 @@ var PriceCalculator = Vue.component('price-calculator', {
   },
   computed: {
     people_t1_with_min: function () {
-      return Math.max(this.people_t1, this.home_rates.people_min - this.people_t2);
+      return Math.max(this.people_t1, this.rates.people_min - this.people_t2);
     },
     people_t1_max: function () {
-      return this.home_rates.people_max - this.people_t2;
+      return this.rates.people_max - this.people_t2;
     },
     people_t2_max: function () {
-      return this.home_rates.people_max - this.people_t1;
+      return this.rates.people_max - this.people_t1;
     },
     nights_max: function () {
       switch (this.occupancy_type) {
         case 'festivity':
-          return 3;
+          return 2;
         default:
           return 30;
       }
     },
     price: function () {
-      var taxes = (this.people_t1 * this.home_rates.tax_t1 * this.nights) +
-        (this.people_t2 * this.home_rates.tax_t2 * this.nights);
-      var additional_costs = (this.home_rates.additional_costs_night * this.nights) +
-        (this.home_rates.rate_flat);
+      var taxes = (this.people_t1 * this.rates.tax_t1 * this.nights) +
+        (this.people_t2 * this.rates.tax_t2 * this.nights);
+      var additional_costs = (this.rates.additional_costs_night * (this.nights + 1)) +
+        (this.rates.rate_flat);
       var price = 0
       switch (this.occupancy_type) {
         case 'festivity':
-          price = this.home_rates.fixed_price[this.nights - 1];
+          price = this.rates.fixed_price[this.nights];
           break;
         default:
-          price = (this.people_t1_with_min * this.home_rates.rate_t1 * this.nights) +
-            (this.people_t2 * this.home_rates.rate_t2 * this.nights);
+          price = (this.people_t1_with_min * this.rates.rate_t1 * this.nights) +
+            (this.people_t2 * this.rates.rate_t2 * this.nights);
       }
       return (price + taxes + additional_costs) * this.price_factor;
     },
@@ -100,8 +111,4 @@ var PriceCalculator = Vue.component('price-calculator', {
       return this.price.toFixed(2);
     }
   }
-})
-
-new Vue({
-	components: { PriceCalculator }
-}).$mount('#pricecalc');
+});
